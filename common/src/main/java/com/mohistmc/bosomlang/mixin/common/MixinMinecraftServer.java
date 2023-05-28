138d346d25ef17d8027cddad01ae32e1156fdb02
1798643961 <1798643961@qq.com>
2023-05-29 00:56:25 +0800

    fix mixin desc

--- common/src/main/java/com/mohistmc/bosomlang/mixin/common/MixinMinecraftServer.java
+++ common/src/main/java/com/mohistmc/bosomlang/mixin/common/MixinMinecraftServer.java
@@ -9,46 +9,46 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(MinecraftServer.class)
 public class MixinMinecraftServer {
 
-    @ModifyConstant(method = "prepareLevels", remap = false, constant = @Constant(stringValue = "Preparing start region for dimension {}"))
+    @ModifyConstant(method = "prepareLevels(Lnet/minecraft/server/level/progress/ChunkProgressListener;)V", remap = false, constant = @Constant(stringValue = "Preparing start region for dimension {}"))
     private String bosom$prepRegion(String constant) {
         return ServerHandleSwitcher.getMSG().get("server.region.prepare");
     }
 
-    @ModifyConstant(method = "saveAllChunks", constant = @Constant(stringValue = "Saving chunks for level '{}'/{}"))
+    @ModifyConstant(method = "saveAllChunks(ZZZ)Z", constant = @Constant(stringValue = "Saving chunks for level '{}'/{}"))
     private String bosom$localSaveChunk(String constant){
         return ServerHandleSwitcher.getMSG().get("server.chunk.saving");
     }
 
-    @ModifyConstant(method = "initializeKeyPair", constant = @Constant(stringValue = "Generating keypair"))
+    @ModifyConstant(method = "initializeKeyPair()V", constant = @Constant(stringValue = "Generating keypair"))
     private String bosom$localKeyPair(String constant){
         return ServerHandleSwitcher.getMSG().get("server.key.pair");
     }
 
-    @ModifyConstant(method = "saveAllChunks", constant = @Constant(stringValue = "ThreadedAnvilChunkStorage ({}): All chunks are saved"))
+    @ModifyConstant(method = "saveAllChunks(ZZZ)Z", constant = @Constant(stringValue = "ThreadedAnvilChunkStorage ({}): All chunks are saved"))
     private String bosom$localSaveAnvil(String constant){
         return ServerHandleSwitcher.getMSG().get("server.chunk.saved");
     }
 
-    @ModifyConstant(method = "saveAllChunks", constant = @Constant(stringValue = "ThreadedAnvilChunkStorage: All dimensions are saved"))
+    @ModifyConstant(method = "saveAllChunks(ZZZ)Z", constant = @Constant(stringValue = "ThreadedAnvilChunkStorage: All dimensions are saved"))
     private String bosom$localSaveAnvil0(String constant){
         return ServerHandleSwitcher.getMSG().get("server.dimension.saved");
     }
 
-    @ModifyConstant(method = "stopServer", constant = @Constant(stringValue = "Stopping server"))
+    @ModifyConstant(method = "stopServer()V", constant = @Constant(stringValue = "Stopping server"))
     private String bosom$localStoppingServer(String constant){
         return ServerHandleSwitcher.getMSG().get("server.stopping");
     }
 
-    @ModifyConstant(method = "stopServer", constant = @Constant(stringValue = "Saving players"))
+    @ModifyConstant(method = "stopServer()V", constant = @Constant(stringValue = "Saving players"))
     private String bosom$localSavePlayer(String constant){
         return ServerHandleSwitcher.getMSG().get("server.saving.player");
     }
 
-    @ModifyConstant(method = "stopServer", constant = @Constant(stringValue = "Saving worlds"))
+    @ModifyConstant(method = "stopServer()V", constant = @Constant(stringValue = "Saving worlds"))
     private String bosom$localSaveWorld(String constant){
         return ServerHandleSwitcher.getMSG().get("server.saving.world");
     }
-    @ModifyConstant(method = "stopServer", constant = @Constant(stringValue = "Can't keep up! Is the server overloaded? Running {}ms or {} ticks behind"))
+    @ModifyConstant(method = "stopServer()V", constant = @Constant(stringValue = "Can't keep up! Is the server overloaded? Running {}ms or {} ticks behind"))
     private String bosom$localCKP(String constant){
         return ServerHandleSwitcher.getMSG().get("server.hold.ckp");
     }
