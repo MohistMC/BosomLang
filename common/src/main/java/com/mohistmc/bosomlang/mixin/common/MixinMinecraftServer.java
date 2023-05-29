464f10b174d53ced3dfa89a30f1afd8994194c35
1798643961 <1798643961@qq.com>
2023-05-29 11:28:26 +0800

    try fix

--- common/src/main/java/com/mohistmc/bosomlang/mixin/common/MixinMinecraftServer.java
+++ common/src/main/java/com/mohistmc/bosomlang/mixin/common/MixinMinecraftServer.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(MinecraftServer.class)
+@Mixin(value = MinecraftServer.class, remap = false)
 public class MixinMinecraftServer {
 
     @ModifyConstant(method = "prepareLevels", remap = false, constant = @Constant(stringValue = "Preparing start region for dimension {}"))
